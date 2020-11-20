bc03972b030b49f3fb3c51a961fbaa5ca86a66d3
Aven Bross <kieroda@gmail.com>
2020-11-19 16:48:01 -0800

    Added missing dependecny and updated makefile.

--- ionic_client/src/pages/Game.tsx
+++ ionic_client/src/pages/Game.tsx
@@ -10,53 +10,96 @@ type GameProps = {
 type GameState = {
 };
 
+class GameThree {
+  scene : THREE.Scene;
+  camera : THREE.PerspectiveCamera;
+  renderer : THREE.WebGLRenderer;
+
+  constructor(width : number, height : number) {
+	this.scene = new THREE.Scene();
+	this.camera = new THREE.PerspectiveCamera(
+	  75, width/height, 0.1, 1000 );
+	this.renderer = new THREE.WebGLRenderer();
+
+	this.renderer.setSize(width, height);
+  }
+
+  resize(width : number, height : number) {
+	this.camera.aspect = width / height;
+	this.camera.updateProjectionMatrix()
+	this.renderer.setSize(width, height);
+  }
+}
+
 class Game extends Component<GameProps, GameState> {
-    componentDidMount() {
-        var scene = new THREE.Scene();
-        var camera = new THREE.PerspectiveCamera(
-                75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-        var renderer = new THREE.WebGLRenderer();
+	gameThree : GameThree;
 
-        renderer.setSize(window.innerWidth, window.innerHeight);
+	constructor(props : GameProps) {
+	  super(props);
+	  this.gameThree = new GameThree(10, 10);
+	}
 
-        var gl_element = document.getElementById("gl_render")!;
-        gl_element.appendChild(renderer.domElement);
+	componentDidMount() {
+		var gl_element = document.getElementById("webgl-container")!;
 
-        var geometry = new THREE.BoxGeometry(1, 1, 1 );
-        var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-        var cube = new THREE.Mesh(geometry, material);
+		gl_element.appendChild(this.gameThree.renderer.domElement);
 
-        var gridXZ = new THREE.GridHelper(100, 10);
-	gridXZ.setColors( new THREE.Color(0x006600), new THREE.Color(0x006600) );
-	gridXZ.position.set( 100,0,100 );
-	scene.add(gridXZ);
+		var geometry = new THREE.BoxGeometry(1, 1, 1 );
+		var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+		var cube = new THREE.Mesh(geometry, material);
+		cube.position.set(0.5, 0.5, 0.5);
 
-        scene.add(cube);
-        camera.position.z = 5;
+		this.gameThree.scene.add(cube);
+		this.gameThree.camera.position.z = 9;
 
-        var animate = function () {
-            requestAnimationFrame(animate);
-            cube.rotation.x += 0.05;
-            cube.rotation.y += 0.05;
-            renderer.render(scene, camera);
-        };
-        animate();
+		var animate = () => {
+			requestAnimationFrame(animate);
+			//cube.rotation.x += 0.05;
+			//cube.rotation.y += 0.05;
+			this.gameThree.renderer.render(
+				this.gameThree.scene,
+				this.gameThree.camera
+			  );
+		};
+		animate();
+    
+    window.addEventListener('resize', this.checkCanvasSize.bind(this));
+
+    this.checkCanvasSize();
+	}
+
+  // Wait for the dom to be rendered and size canvas appropriately
+	checkCanvasSize() { 
+	  var gl_element = document.getElementById("webgl-container")!;
+	  let width = gl_element.offsetWidth;
+	  let height = gl_element.offsetHeight; 
+
+    if(gl_element.offsetWidth) {
+      this.gameThree.resize(width, height);
     }
-
-    render() {
-        return (
-            <IonPage>
-                <IonHeader>
-                    <IonToolbar>
-                        <IonTitle>Three Scene</IonTitle>
-                    </IonToolbar>
-                </IonHeader>
-                <IonContent fullscreen>
-                    <div id="gl_render"></div>
-                </IonContent>
-            </IonPage>
-        )
+    else {
+      setTimeout(this.checkCanvasSize.bind(this), 100);	 
     }
+	}
+
+	componentDidUpdate() { 
+    this.checkCanvasSize();
+	}
+
+	render() {
+		return (
+			<IonPage>
+				<IonHeader>
+					<IonToolbar>
+						<IonTitle>Three Scene</IonTitle>
+					</IonToolbar>
+				</IonHeader>
+				<IonContent fullscreen>
+					<div id="webgl-container"></div>
+				</IonContent>
+			</IonPage>
+		)
+	}
 }
 
 export default Game;

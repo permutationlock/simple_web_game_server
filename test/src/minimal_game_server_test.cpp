5554c017f93f1f25bb4e50fb0b8c9f4fb7bebcd7
Aven Bross <kieroda@gmail.com>
2020-12-18 11:54:34 -0800

    Finished minimal server tests, now passing.

--- test/src/minimal_game_server_test.cpp
+++ test/src/minimal_game_server_test.cpp
@@ -11,110 +11,36 @@
 #include <json_traits/nlohmann_traits.hpp>
 #include <model_games/minimal_game.h>
 
-#include <websocketpp/config/asio_no_tls.hpp>
-#include <websocketpp/config/asio_no_tls_client.hpp>
+#include <websocketpp_configs/asio_no_logs.hpp>
+#include <websocketpp_configs/asio_client_no_logs.hpp>
 
 #include <thread>
 #include <functional>
 #include <sstream>
 #include <chrono>
 
-struct server_config : public websocketpp::config::asio {
-  typedef server_config type;
-  typedef websocketpp::config::asio base;
-
-  typedef base::concurrency_type concurrency_type;
-
-  typedef base::request_type request_type;
-  typedef base::response_type response_type;
-
-  typedef base::message_type message_type;
-  typedef base::con_msg_manager_type con_msg_manager_type;
-  typedef base::endpoint_msg_manager_type endpoint_msg_manager_type;
-
-  typedef base::alog_type alog_type;
-  typedef base::elog_type elog_type;
-
-  typedef base::rng_type rng_type;
-
-  struct transport_config : public base::transport_config {
-    typedef type::concurrency_type concurrency_type;
-    typedef type::alog_type alog_type;
-    typedef type::elog_type elog_type;
-    typedef type::request_type request_type;
-    typedef type::response_type response_type;
-    typedef websocketpp::transport::asio::basic_socket::endpoint socket_type;
-  };
-
-  typedef websocketpp::transport::asio::endpoint<transport_config>
-    transport_type;
-
-  static const websocketpp::log::level elog_level = 
-    websocketpp::log::elevel::none;
-
-  static const websocketpp::log::level alog_level =
-    websocketpp::log::alevel::none;
-};
-
-struct client_config : public websocketpp::config::core_client {
-  typedef client_config type;
-  typedef websocketpp::config::core_client base;
-
-  typedef base::concurrency_type concurrency_type;
-
-  typedef base::request_type request_type;
-  typedef base::response_type response_type;
-
-  typedef base::message_type message_type;
-  typedef base::con_msg_manager_type con_msg_manager_type;
-  typedef base::endpoint_msg_manager_type endpoint_msg_manager_type;
-
-  typedef base::alog_type alog_type;
-  typedef base::elog_type elog_type;
-
-  typedef base::rng_type rng_type;
-
-  struct transport_config : public base::transport_config {
-    typedef type::concurrency_type concurrency_type;
-    typedef type::alog_type alog_type;
-    typedef type::elog_type elog_type;
-    typedef type::request_type request_type;
-    typedef type::response_type response_type;
-    typedef websocketpp::transport::asio::basic_socket::endpoint
-      socket_type;
-  };
-
-  typedef websocketpp::transport::asio::endpoint<transport_config>
-    transport_type;
-
-  static const websocketpp::log::level elog_level = 
-    websocketpp::log::elevel::none;
-
-  static const websocketpp::log::level alog_level =
-    websocketpp::log::alevel::none;
-};
-
-TEST_CASE("the server should start and shutdown without exceptions") {
+TEST_CASE("players should be able to connect to the server with no errors") {
   using namespace std::chrono_literals;
 
   typedef jwt_game_server::base_client<
-      client_config
+      asio_client_no_logs
     > minimal_game_client;
 
   typedef jwt_game_server::game_server<
       minimal_game,
       jwt::default_clock,
       nlohmann_traits,
-      server_config
+      asio_no_logs
     > minimal_game_server;
 
-  // setup logging sink to test logs
+  typedef jwt::basic_claim<nlohmann_traits> claim;
+
+  // setup logging sink to track errors
   std::ostringstream oss;
   auto ostream_sink = std::make_shared<spdlog::sinks::ostream_sink_mt> (oss);
   auto logger = std::make_shared<spdlog::logger>("my_logger", ostream_sink);
   spdlog::set_default_logger(logger);
   spdlog::set_level(spdlog::level::err);
-  //spdlog::set_level(spdlog::level::trace);
 
   // create a jwt verifier
   jwt::verifier<jwt::default_clock, nlohmann_traits> 
@@ -122,56 +48,135 @@ TEST_CASE("the server should start and shutdown without exceptions") {
   verifier.allow_algorithm(jwt::algorithm::hs256("secret"))
     .with_issuer("jwt-gs-test");
 
-  minimal_game_server gs(verifier);
+  SUBCASE("games start when players connect and die when they leave") {
+    minimal_game_server gs(verifier, 10000ms);
 
-  std::thread server_thr(bind(&minimal_game_server::run, &gs, 9090));
+    std::thread server_thr(bind(&minimal_game_server::run, &gs, 9090, true));
 
-  // wait for the server to start
-  while(!gs.is_running()) {
-    std::this_thread::sleep_for(10ms);
+    // wait for the server to start
+    while(!gs.is_running()) {
+      std::this_thread::sleep_for(10ms);
+    }
+   
+    // bind a thread to process messages
+    std::thread msg_process_thr(bind(&minimal_game_server::process_messages,&gs));
+
+    // bind a thread to update all running games at regular time steps
+    std::thread game_thr(bind(&minimal_game_server::update_games,&gs));
+
+    std::vector<minimal_game_client> clients;
+    std::vector<std::thread> client_threads;
+
+    const int PLAYER_COUNT = 200;
+    for(std::size_t i = 0; i < PLAYER_COUNT; i++) {
+      minimal_game::player_id player = i;
+      std::vector<minimal_game::player_id> players = { i };
+      nlohmann_traits::json data = { { "players", players } };
+      auto token = jwt::create<nlohmann_traits>()
+        .set_issuer("jwt-gs-test")
+        .set_payload_claim("id", claim(player))
+        .set_payload_claim("data", claim(data))
+        .sign(jwt::algorithm::hs256{"secret"});
+
+      clients.push_back(minimal_game_client{"ws://localhost:9090", token});
+    }
+
+    for(std::size_t i = 0; i < PLAYER_COUNT; i++) {
+      std::thread client_thr(bind(&minimal_game_client::connect, &(clients[i])));
+
+      while(!clients[i].is_running()) {
+        std::this_thread::sleep_for(1ms);
+      }
+
+      client_threads.push_back(std::move(client_thr));
+    }
+
+    std::this_thread::sleep_for(1000ms);
+
+    CHECK(gs.get_player_count() == PLAYER_COUNT);
+   
+    for(std::size_t i = 0; i < PLAYER_COUNT; i++) {
+      clients[i].disconnect();
+    }
+
+    for(std::size_t i = 0; i < PLAYER_COUNT; i++) {
+      client_threads[i].join();
+    }
+
+    std::this_thread::sleep_for(1000ms);
+
+    CHECK(gs.get_player_count() == 0);
+
+    gs.stop();
+
+    msg_process_thr.join();
+    game_thr.join();
+    server_thr.join();
+
+    CHECK(oss.str().empty());
   }
- 
-  // bind a thread to process messages
-  std::thread msg_process_thr(bind(&minimal_game_server::process_messages,&gs));
 
-  // bind a thread to update all running games at regular time steps
-  std::thread game_thr(bind(&minimal_game_server::update_games,&gs));
+  SUBCASE("players should be disconnected when games end") {
+    // server with fast loop that notices games are done
+    minimal_game_server gs(verifier, 10ms);
 
-  typedef jwt::basic_claim<nlohmann_traits> claim;
+    std::thread server_thr(bind(&minimal_game_server::run, &gs, 9090, true));
 
-  unsigned int player = 17;
-  std::vector<unsigned int> players = { 17 };
-  nlohmann_traits::json data = { { "players", players } };
-  auto token = jwt::create<nlohmann_traits>()
-    .set_issuer("jwt-gs-test")
-    .set_payload_claim("id", claim(player))
-    .set_payload_claim("data", claim(data))
-    .sign(jwt::algorithm::hs256{"secret"});
+    // wait for the server to start
+    while(!gs.is_running()) {
+      std::this_thread::sleep_for(10ms);
+    }
+   
+    // bind a thread to process messages
+    std::thread msg_process_thr(bind(&minimal_game_server::process_messages,&gs));
 
-  minimal_game_client c("ws://localhost:9090", token);
-  std::thread client_thr(bind(&minimal_game_client::connect, &c));
+    // bind a thread to update all running games at regular time steps
+    std::thread game_thr(bind(&minimal_game_server::update_games,&gs));
 
-  while(!c.is_running()) {
+    std::vector<minimal_game_client> clients;
+    std::vector<std::thread> client_threads;
+
+    const int PLAYER_COUNT = 200;
+    for(std::size_t i = 0; i < PLAYER_COUNT; i++) {
+      minimal_game::player_id player = i;
+      std::vector<minimal_game::player_id> players = { i };
+      nlohmann_traits::json data = { { "players", players } };
+      auto token = jwt::create<nlohmann_traits>()
+        .set_issuer("jwt-gs-test")
+        .set_payload_claim("id", claim(player))
+        .set_payload_claim("data", claim(data))
+        .sign(jwt::algorithm::hs256{"secret"});
+
+      clients.push_back(minimal_game_client{"ws://localhost:9090", token});
+    }
+
+    for(std::size_t i = 0; i < PLAYER_COUNT; i++) {
+      std::thread client_thr(bind(&minimal_game_client::connect, &(clients[i])));
+      client_threads.push_back(std::move(client_thr));
+    }
+
+    std::this_thread::sleep_for(1000ms);
+
+    CHECK(gs.get_player_count() == 0);
+   
+    for(std::size_t i = 0; i < PLAYER_COUNT; i++) {
+      clients[i].disconnect();
+    }
+
+    for(std::size_t i = 0; i < PLAYER_COUNT; i++) {
+      client_threads[i].join();
+    }
+
     std::this_thread::sleep_for(10ms);
-  }
 
-  std::this_thread::sleep_for(10ms);
+    gs.stop();
 
-  CHECK(gs.get_player_count() == 1);
-  
-  c.disconnect();
-
-  while(c.is_running()) {
     std::this_thread::sleep_for(10ms);
+
+    msg_process_thr.join();
+    game_thr.join();
+    server_thr.join();
+
+    CHECK(oss.str().empty());
   }
-
-  client_thr.join();
-
-  std::this_thread::sleep_for(10ms);
-
-  gs.stop();
-
-  msg_process_thr.join();
-  game_thr.join();
-  server_thr.join();
 }
--- test/src/minimal_game_test.cpp
+++ test/src/minimal_game_test.cpp
@@ -6,126 +6,132 @@
 #include <map>
 #include <set>
 
-namespace {
+TEST_CASE("minimal games are constructed from json { \"players\": [..] }") {
   using json = nlohmann::json;
+  using std::map;
+  using std::vector;
+
+  minimal_game empty_players{json::parse("{ \"players\": [] }")};
+  CHECK(empty_players.is_valid() == true);
+  CHECK(empty_players.get_player_list().size() == 0);
+
+  minimal_game one_player{json::parse("{ \"players\": [14] }")};
+  CHECK(one_player.is_valid() == true);
+  CHECK(one_player.get_player_list().size() == 1);
+  CHECK(one_player.get_player_list()[0] == 14);
+
+  minimal_game fourteen_players{
+    json::parse("{ \"players\": [2, 19, 218, 14131, 123, 0, 9] }")};
+  CHECK(fourteen_players.is_valid() == true);
+  CHECK(fourteen_players.get_player_list().size() == 7);
+  CHECK(fourteen_players.get_player_list()[0] == 2);
+  CHECK(fourteen_players.get_player_list()[1] == 19);
+  CHECK(fourteen_players.get_player_list()[2] == 218);
+  CHECK(fourteen_players.get_player_list()[3] == 14131);
+  CHECK(fourteen_players.get_player_list()[4] == 123);
+  CHECK(fourteen_players.get_player_list()[5] == 0);
+  CHECK(fourteen_players.get_player_list()[6] == 9);
+
+  minimal_game empty_json{json::parse("{}")};
+  CHECK(empty_json.is_valid() == false);
+
+  minimal_game non_list_players{json::parse("{ \"players\": 21 }")};
+  CHECK(non_list_players.is_valid() == false);
+
+  minimal_game string_players{json::parse("{ \"players\": [\"a\"] }")};
+  CHECK(string_players.is_valid() == false);
+
+  minimal_game object_players{json::parse("{ \"players\": [{ \"id\": 21 }] }")};
+  CHECK(object_players.is_valid() == false);
+}
+
+TEST_CASE("minimal games should be done and not have messages") {
+  using json = nlohmann::json;
+
+  minimal_game one_player{json::parse("{ \"players\": [83] }")};
+  CHECK(one_player.is_done() == true);
+  CHECK(one_player.has_message() == false);
+}
+
+TEST_CASE("matchmaking data game should track list of players and dump to json") {
+  using json = nlohmann::json;
+  using std::vector;
+
+  typedef minimal_matchmaking_data::player_id player_id;
+  typedef minimal_matchmaking_data::game game;
+
+  game g{};
+
+  SUBCASE("empty game should have an empty list") {
+    CHECK(g.get_player_list().size() == 0);
+    CHECK(g.to_json() == json::parse("{\"players\":[]}"));
+  }
+
+  SUBCASE("game with two players should store correctly and parse to json") {
+    g.add_player(75);
+    g.add_player(34);
+    vector<player_id> pl = g.get_player_list();
+    CHECK(pl.size() == 2);
+    CHECK(pl[0] == 75);
+    CHECK(pl[1] == 34);
+    CHECK(g.to_json() == json::parse("{\"players\":[75,34]}"));
+  }
+}
+
+TEST_CASE("matchmaking data match function should pair players in order") {
   using std::set;
   using std::map;
   using std::vector;
 
-  TEST_CASE("minimal games are constructed from json { \"players\": [..] }") {
-    minimal_game empty_players{json::parse("{ \"players\": [] }")};
-    CHECK(empty_players.is_valid() == true);
-    CHECK(empty_players.get_player_list().size() == 0);
+  typedef minimal_matchmaking_data::player_id player_id;
+  typedef minimal_matchmaking_data::player_data player_data;
+  typedef minimal_matchmaking_data::game game;
 
-    minimal_game one_player{json::parse("{ \"players\": [14] }")};
-    CHECK(one_player.is_valid() == true);
-    CHECK(one_player.get_player_list().size() == 1);
-    CHECK(one_player.get_player_list()[0] == 14);
+  map<player_id, player_data> player_map;
 
-    minimal_game fourteen_players{
-      json::parse("{ \"players\": [2, 19, 218, 14131, 123, 0, 9] }")};
-    CHECK(fourteen_players.is_valid() == true);
-    CHECK(fourteen_players.get_player_list().size() == 7);
-    CHECK(fourteen_players.get_player_list()[0] == 2);
-    CHECK(fourteen_players.get_player_list()[1] == 19);
-    CHECK(fourteen_players.get_player_list()[2] == 218);
-    CHECK(fourteen_players.get_player_list()[3] == 14131);
-    CHECK(fourteen_players.get_player_list()[4] == 123);
-    CHECK(fourteen_players.get_player_list()[5] == 0);
-    CHECK(fourteen_players.get_player_list()[6] == 9);
-
-    minimal_game empty_json{json::parse("{}")};
-    CHECK(empty_json.is_valid() == false);
-
-    minimal_game non_list_players{json::parse("{ \"players\": 21 }")};
-    CHECK(non_list_players.is_valid() == false);
-
-    minimal_game string_players{json::parse("{ \"players\": [\"a\"] }")};
-    CHECK(string_players.is_valid() == false);
-
-    minimal_game object_players{json::parse("{ \"players\": [{ \"id\": 21 }] }")};
-    CHECK(object_players.is_valid() == false);
+  SUBCASE("empty map should return empty list of games") {
+    vector<game> games = minimal_matchmaking_data::match(player_map);
+    CHECK(games.size() == 0);
   }
 
-  TEST_CASE("minimal games should be done and not have messages") {
-    minimal_game one_player{json::parse("{ \"players\": [83] }")};
-    CHECK(one_player.is_done() == true);
-    CHECK(one_player.has_message() == false);
-  }
+  SUBCASE("map with two players should return one game") {
+    player_map[43] = player_data{};
+    player_map[102] = player_data{};
+    vector<game> games = minimal_matchmaking_data::match(player_map);
+    CHECK(games.size() == 1);
 
-  TEST_CASE("matchmaking data game should track list of players and dump to json") {
-    typedef minimal_matchmaking_data::player_id player_id;
-    typedef minimal_matchmaking_data::game game;
-
-    game g{};
-
-    SUBCASE("empty game should have an empty list") {
-      CHECK(g.get_player_list().size() == 0);
-      CHECK(g.to_json() == json::parse("{\"players\":[]}"));
+    set<player_id> players;
+    for(const game& game : games) {
+      for(player_id id : game.get_player_list()) {
+        players.insert(id);
+      }
     }
-
-    SUBCASE("game with two players should store correctly and parse to json") {
-      g.add_player(75);
-      g.add_player(34);
-      vector<player_id> pl = g.get_player_list();
-      CHECK(pl.size() == 2);
-      CHECK(pl[0] == 75);
-      CHECK(pl[1] == 34);
-      CHECK(g.to_json() == json::parse("{\"players\":[75,34]}"));
+    CHECK(players.size() == 2);
+    for(player_id id : players) {
+      CHECK(player_map.count(id) > 0);
     }
   }
 
-  TEST_CASE("matchmaking data match function should pair players in order") {
-    typedef minimal_matchmaking_data::player_id player_id;
-    typedef minimal_matchmaking_data::player_data player_data;
-    typedef minimal_matchmaking_data::game game;
+  SUBCASE("map with seven players should return three games") {
+    player_map[8] = player_data{};
+    player_map[66] = player_data{};
+    player_map[163] = player_data{};
+    player_map[421] = player_data{};
+    player_map[741] = player_data{};
+    player_map[907] = player_data{};
+    player_map[2001] = player_data{};
+    vector<game> games = minimal_matchmaking_data::match(player_map);
+    CHECK(games.size() == 3);
 
-    map<player_id, player_data> player_map;
-
-    SUBCASE("empty map should return empty list of games") {
-      vector<game> games = minimal_matchmaking_data::match(player_map);
-      CHECK(games.size() == 0);
-    }
-
-    SUBCASE("map with two players should return one game") {
-      player_map[43] = player_data{};
-      player_map[102] = player_data{};
-      vector<game> games = minimal_matchmaking_data::match(player_map);
-      CHECK(games.size() == 1);
-
-      set<player_id> players;
-      for(const game& game : games) {
-        for(player_id id : game.get_player_list()) {
-          players.insert(id);
-        }
-      }
-      CHECK(players.size() == 2);
-      for(player_id id : players) {
-        CHECK(player_map.count(id) > 0);
+    set<player_id> players;
+    for(const game& game : games) {
+      for(player_id id : game.get_player_list()) {
+        players.insert(id);
       }
     }
-
-    SUBCASE("map with seven players should return three games") {
-      player_map[8] = player_data{};
-      player_map[66] = player_data{};
-      player_map[163] = player_data{};
-      player_map[421] = player_data{};
-      player_map[741] = player_data{};
-      player_map[907] = player_data{};
-      player_map[2001] = player_data{};
-      vector<game> games = minimal_matchmaking_data::match(player_map);
-      CHECK(games.size() == 3);
-
-      set<player_id> players;
-      for(const game& game : games) {
-        for(player_id id : game.get_player_list()) {
-          players.insert(id);
-        }
-      }
-      CHECK(players.size() == 6);
-      for(player_id id : players) {
-        CHECK(player_map.count(id) > 0);
-      }
+    CHECK(players.size() == 6);
+    for(player_id id : players) {
+      CHECK(player_map.count(id) > 0);
     }
   }
 }
